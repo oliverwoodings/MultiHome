fea8ffe41cb9d26dba603703ad1cb74216115561
Oli <oliverw92@mac.com>
2011-04-21 03:19:15 +0100

    v0.2 Official Release

--- src/uk/co/oliwali/MultiHome/MultiHome.java
+++ src/uk/co/oliwali/MultiHome/MultiHome.java
@@ -1,6 +1,10 @@
 package uk.co.oliwali.MultiHome;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+
+import javax.persistence.PersistenceException;
 
 import org.bukkit.ChatColor;
 import org.bukkit.World;
@@ -24,9 +28,25 @@ public class MultiHome extends JavaPlugin {
 		name = this.getDescription().getName();
         version = this.getDescription().getVersion();
         permissions = new Permission(this);
-        
+        setupDatabase();
         sendMessage("info", "Version " + version + " enabled!");
 	}
+	
+	private void setupDatabase() {
+        try {
+            getDatabase().find(Home.class).findRowCount();
+        } catch (PersistenceException ex) {
+            System.out.println("Installing database for " + getDescription().getName() + " due to first time usage");
+            installDDL();
+        }
+    }
+
+    @Override
+    public List<Class<?>> getDatabaseClasses() {
+        List<Class<?>> list = new ArrayList<Class<?>>();
+        list.add(Home.class);
+        return list;
+    }
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String args[]) {
 		
@@ -35,30 +55,31 @@ public class MultiHome extends JavaPlugin {
 		World world   = player.getWorld();
 		
 		if (prefix.equalsIgnoreCase("home") && permissions.home(player)) {
-			String command = args[0];
-			
-			//Setting home
-			if (command.equalsIgnoreCase("set")) {
-				Home home = (Home) getDatabase().find(Home.class).where().ieq("name", name).ieq("playerName", player.getName());
-		        if (home == null) {
-		            home = new Home();
-		            home.setPlayer(player.getName());
-		        }
-		        home.setLocation(player.getLocation());
-		        getDatabase().save(home);
-		        sendMessage(player, "`aYour home has been set in world `f" + world.getName());
+			if (args.length > 0) {
+				String command = args[0];
+				
+				//Setting home
+				if (command.equalsIgnoreCase("set")) {
+					Home home = getDatabase().find(Home.class).where().ieq("world", world.getName()).ieq("player", player.getName()).findUnique();
+			        if (home == null) {
+			            home = new Home();
+			            home.setPlayer(player.getName());
+			        }
+			        home.setLocation(player.getLocation());
+			        getDatabase().save(home);
+			        sendMessage(player, "`aYour home has been set in world `f" + world.getName());
+				}
+				
+				//Get help
+				else if (command.equalsIgnoreCase("help")) {
+					sendMessage(player, "`a--------------------`f MultiHome `a--------------------");
+					sendMessage(player, "`a/home`f - Go to your home in the world you are in");
+					sendMessage(player, "`a/home set`f - Set your home in the world you are in");
+				}
 			}
-			
-			//Get help
-			else if (command.equalsIgnoreCase("help")) {
-				sendMessage(player, "`a--------------------`f MultiHome `a--------------------");
-				sendMessage(player, "`a/home`f - Go to your home in the world you are in");
-				sendMessage(player, "`a/home set`f - Set your home in the world you are in");
-			}
-			
 			//Go home
 			else {
-				Home home = (Home) getDatabase().find(Home.class).where().ieq("name", name).ieq("playerName", player.getName());
+				Home home = (Home) getDatabase().find(Home.class).where().ieq("world", world.getName()).ieq("player", player.getName()).findUnique();
 		        if (home == null) {
 		        	sendMessage(player, "`rYou do not have a home set in world `f" + world.getName());
 		        	sendMessage(player, "`fUse `r/home set`f to set a home");
@@ -67,6 +88,7 @@ public class MultiHome extends JavaPlugin {
 		        	player.teleport(home.getLocation());
 		        }
 			}
+			return true;
 			
 		}
 		return false;
