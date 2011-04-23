07cf565ace1179e9119ad338af2fb859789eab92
Oli <oliverw92@mac.com>
2011-04-23 14:01:01 +0100

    Fixed home set, just waiting for Bukkit to fix getDatabase.save()

--- src/uk/co/oliwali/MultiHome/MultiHome.java
+++ src/uk/co/oliwali/MultiHome/MultiHome.java
@@ -61,10 +61,12 @@ public class MultiHome extends JavaPlugin {
 				//Setting home
 				if (command.equalsIgnoreCase("set")) {
 					Home home = getDatabase().find(Home.class).where().ieq("world", world.getName()).ieq("player", player.getName()).findUnique();
-			        if (home == null) {
-			            home = new Home();
-			            home.setPlayer(player.getName());
-			        }
+			        
+					if (home != null) {
+						home = new Home();
+						home.setPlayer(player.getName());
+					}
+					
 			        home.setLocation(player.getLocation());
 			        getDatabase().save(home);
 			        sendMessage(player, "`aYour home has been set in world `f" + world.getName());
